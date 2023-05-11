bf600a12bc954c1b1c9b55c3745b8a28557d2367
saharacode <johanndallmann@gmx.de>
2023-05-11 19:21:09 +0200

    added buttons to switch between all pages of the api

--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -6,16 +6,37 @@ import axios from "axios";
 
 
 export default function CharacterGallery() {
+    const initialUrl = "https://rickandmortyapi.com/api/character";
+    let newUrl = ""
+    const [pageCounter,setPageCounter] = useState(1);
     const [characters, setCharacters] = useState<Character[]>([]) // save characters from api
     const [inputName,setInputName] = useState("");
 
     useEffect(() =>{
-        getAllCharactersFromApi() // call function automatically
+        getAllCharactersFromApi(initialUrl) // call function automatically
     }, []) // with [] make sure that its just one time called (no infinity loop)
 
-    function getAllCharactersFromApi(){
-        axios.get("https://rickandmortyapi.com/api/character")
+    function getAllCharactersFromApi(apiUrl:string){
+        axios.get(apiUrl) // get information from url
             .then(response => setCharacters(response.data.results)); // save all characters from api in useState characters
+    }
+
+    function nextPage() {
+        if(pageCounter<=41){ // make sure that it stops at the last page
+            let nextPageCounter = pageCounter + 1; // keeping pages synchronus with the click on button
+            newUrl = initialUrl + "/?page=" + nextPageCounter;
+            getAllCharactersFromApi(newUrl);
+            setPageCounter(pageCounter+1);
+        }
+    }
+
+    function previousPage() {
+        if (pageCounter >= 2) {
+            let previousPageCounter = pageCounter - 1;
+            newUrl = initialUrl + "/?page=" + previousPageCounter;
+            getAllCharactersFromApi(newUrl)
+            setPageCounter(pageCounter - 1);
+        }
     }
 
     function useTextInput(event: React.FormEvent<HTMLInputElement>) {
@@ -35,6 +56,10 @@ export default function CharacterGallery() {
             <div id="window">
                 <h3>Please type in the name, you are looking for:</h3>
                 <input placeholder="Search..." type="text" value={inputName} onInput={useTextInput}/>
+            </div>
+            <div>
+                <button onClick={previousPage}>Previous Page</button>
+                <button onClick={nextPage}>Next Page</button>
             </div>
             <CharacterCard characters={filterCharacters()} ></CharacterCard>
         </div>
