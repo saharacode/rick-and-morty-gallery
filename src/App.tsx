eb647e10f3534bb4953e9d68b19ef03052c18520
saharacode <johanndallmann@gmx.de>
2023-05-15 14:32:44 +0200

    swap pages moved to app.tsx, details works now for every page

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ import axios from "axios";
 import {Character} from "./Character";
 
 export default function App() {
-
     const initialUrl = "https://rickandmortyapi.com/api/character";
+    let newUrl = ""
+    const [pageCounter,setPageCounter] = useState(1);
     const [characters, setCharacters] = useState<Character[]>([]) // save characters from api
 
     useEffect(() =>{
@@ -20,14 +21,36 @@ export default function App() {
             .then(response => setCharacters(response.data.results)); // save all characters from api in useState characters
     }
 
+    function nextPage() {
+        if(pageCounter<=41){ // make sure that it stops at the last page
+            let nextPageCounter = pageCounter + 1; // keeping pages synchronus with the click on button
+            newUrl = initialUrl + "/?page=" + nextPageCounter;
+            getAllCharactersFromApi(newUrl);
+            setPageCounter(pageCounter+1);
+        }
+    }
+
+    function previousPage() {
+        if (pageCounter >= 2) {
+            let previousPageCounter = pageCounter - 1;
+            newUrl = initialUrl + "/?page=" + previousPageCounter;
+            getAllCharactersFromApi(newUrl)
+            setPageCounter(pageCounter - 1);
+        }
+    }
+
 
   return (
     <div className="headline">
         <header className="headline">
-            <h1>Welcome to the Rick and Morty App</h1>
+            <h1>The Rick and Morty App</h1>
+            <div>
+                <button onClick={previousPage}>Previous Page</button>
+                <button onClick={nextPage}>Next Page</button>
+            </div>
         </header>
         <Routes>
-            <Route path={"/home"} element={<CharacterGallery/>}></Route>
+            <Route path={"/home"} element={<CharacterGallery characters={characters}/>}></Route>
             <Route path={"/character/:id"} element = {<CharacterDetailCard characters={characters}/>}></Route>
         </Routes>
 
